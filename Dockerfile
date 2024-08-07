eaac545f1357680ab2290ebc71e67535cd75f284
Kaushik Dey <kaushikdey647@gmail.com>
2024-08-07 19:59:28 +0530

    [dev]: Kaushik starts here

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:20.11
+
+# Create app directory
+WORKDIR /app
+
+# Install app dependencies
+COPY package.json .
+
+RUN npm install
+
+# Bundle app source
+
+COPY . .
+
+EXPOSE 8000
+
+CMD ["npm", "start"]
--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ import news from "./utils/scraper.js";
 
 const app = express();
 const port = 3000;
-app.use(cors({origin: "http://localhost:5173",
-  credentials: true}))
+// app.use(cors({origin: "http://localhost:5173",
+//   credentials: true}))
 
 
 app.use(bodyParser.json());
